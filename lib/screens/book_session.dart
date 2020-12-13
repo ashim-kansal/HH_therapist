f3fd051fed68ebfc404c13e445f2dc31fd7d0cc5
ashimkansal <kansal.136@gmail.com>
2020-12-13 17:13:46 +0530

    worked on sessions

--- lib/screens/book_session.dart
+++ lib/screens/book_session.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app/screens/sessions.dart';
+import 'package:flutter_app/widgets/MyScaffoldWidget.dart';
+import 'package:flutter_app/widgets/mywidgets.dart';
 import 'package:flutter_app/widgets/schedule_widgets.dart';
 import 'package:flutter_app/widgets/tharapist_cell.dart';
 
@@ -15,38 +19,24 @@ class BookSessionState extends State<BookSessionPage>{
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Schedule', style: TextStyle(color: Colors.white)),
-          centerTitle: true,
-          iconTheme: IconThemeData(
-            color: Colors.white, //change your color here
-          ),
-          backgroundColor: Theme.of(context).accentColor,
-          elevation: 0,
+    return MyWidget(title: 'Schedule', child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        TharapistCell(name: 'Abc', role: 'Abcd', showBook: false, onClick: (){},),
+        Container(height:50, child: SessionDateWidget()),
+        SizedBox(height: 10,),
+        Expanded(child: GridViewWidget(),),
+        Container(
+            child: HHButton(title: 'Save', type: 4, isEnable: true,onClick: (){
+              Navigator.pushNamed(context, SessionPage.RouteName);
+            },)
+        )
+      ],
+    )
+      // backgroundColor: Colors.white,
+      // This trailing comma makes auto-formatting nicer for build methods.
 
-        ),
-
-        body: Material(
-            color: Theme.of(context).accentColor,
-            child: Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(30.0),
-                    topLeft: Radius.circular(30.0),
-                  ),
-                  color: Colors.white),
-              child: Column(
-                children: [
-                  TharapistCell(name: 'Abc', role: 'Abcd', showBook: false, onClick: (){},),
-                  Expanded(child: SessionDateWidget()),
-                  Expanded(child: GridViewWidget(),)
-                ],
-              )
-              // backgroundColor: Colors.white,
-              // This trailing comma makes auto-formatting nicer for build methods.
-            )));
+    );
 
   }
 }
--- lib/widgets/schedule_widgets.dart
+++ lib/widgets/schedule_widgets.dart
@@ -8,54 +8,106 @@ class GridViewWidget extends StatelessWidget{
     return GridView.count(
       // Create a grid with 2 columns. If you change the scrollDirection to
       // horizontal, this produces 2 rows.
-      crossAxisCount: 4,
-      childAspectRatio: 2,
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      // Generate 100 widgets that display their index in the List.
-      children: List.generate(100, (index) {
-        return Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(4.0)),
-            color: HH_Colors.grey_F2F2F2,
-          ),
-          padding: EdgeInsets.all(15),
-          child: Center(child: Text(
-            'Item $index',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: HH_Colors.grey_707070),
-          ),),
-        );
-      })
+        crossAxisCount: 4,
+        childAspectRatio: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(100, (index) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(4.0)),
+              color: HH_Colors.grey_F2F2F2,
+            ),
+            padding: EdgeInsets.all(10),
+            child: Center(child: Text(
+              'Item $index',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: HH_Colors.grey_707070),
+            ),),
+          );
+        })
     );
   }
 }
 
 
-class SessionDateWidget extends StatelessWidget{
+class SessionDateWidget extends StatefulWidget{
+  var list = [];
+
+
+  @override
+  State<StatefulWidget> createState() => SessionDateWidgetState();
+
+}
+
+class SessionDateWidgetState extends State<SessionDateWidget>{
+
+  void populateData() {
+    for (int i = 0; i < 10; i++)
+      widget.list.add(ListItem<String>("$i Nov\nMon"));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.list??List();
+
+    populateData();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      // Create a grid with 2 columns. If you change the scrollDirection to
-      // horizontal, this produces 2 rows.
-      crossAxisCount: 4,
-      childAspectRatio: 2,
-      shrinkWrap: true,
-      // Generate 100 widgets that display their index in the List.
-      children: List.generate(10, (index) {
-        return Container(
-          decoration: BoxDecoration(
-            color: HH_Colors.grey_F2F2F2,
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: widget.list.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(3)),
+              color: widget.list[index].isSelected ? HH_Colors.primaryColor : Colors.white,
+
+            ),
+            padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+            child: Center(
+              child: Text(
+                widget.list[index].data,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: widget.list[index].isSelected? Colors.white : HH_Colors.grey_707070),
+              ),),
           ),
-          padding: EdgeInsets.all(15),
-          child: Center(child: Text(
-            '16 Nov\nMon',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: HH_Colors.grey_707070),
-          ),),
+          onTap: (){
+            setItemSelected(index);
+          },
         );
-      })
+
+
+
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
     );
   }
+
+  void setItemSelected(int index) {
+    setState(() {
+      for (int i = 0; i < widget.list.length; i++){
+        if(index == i) {
+          widget.list[i].isSelected = true;
+        }else{
+          widget.list[i].isSelected = false;
+        }
+      }
+
+
+    });
+  }
+}
+
+class ListItem<String> {
+  bool isSelected = false; //Selection property to highlight or not
+  String data; //Data of the user
+  ListItem(this.data); //Constructor to assign the data
 }
