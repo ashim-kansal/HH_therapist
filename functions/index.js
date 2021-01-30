fd2a8b02e67ef0b3b1bf496fa4d56cacd5cbde0d
ashimkansal <kansal.136@gmail.com>
2021-01-30 17:02:52 +0530

    working on agora

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().functions);
+
+var newData;
+
+exports.myTrigger = functions.firestore.document('notifications/{messageid}').onCreate(async (snapshot, context) => {
+
+
+    if (snapshot.empty) {
+        console.log('No Devices');
+        return;
+    }
+
+    newData = snapshot.data();
+
+    var tokens = [];
+
+    tokens.push(newData['deviceid']);
+
+    var payload = {
+        notification: {
+            title: newData['sendby'],
+            body: newData['message'],
+            sound: 'default',
+        }
+    };
+
+    try {
+        const response = await admin.messaging().sendToDevice(tokens, payload);
+        console.log('Notification sent successfully');
+    } catch (err) {
+        console.log(err);
+    }
+});
